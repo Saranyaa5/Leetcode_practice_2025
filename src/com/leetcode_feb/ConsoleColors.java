764f577c7a26676bfd2856a39e30b3aa06585cba
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-02-26 21:13:40 +0530

    Update ConsoleColors.java

--- src/com/leetcode_feb/ConsoleColors.java
+++ src/com/leetcode_feb/ConsoleColors.java
@@ -1,1 +1,16 @@
+package com.leetcode_feb;
+
+public class ConsoleColors {
+	public static final String BOLD = "\u001B[1m";
+	
+    public static final String RESET = "\u001B[0m";  
+    public static final String RED = "\u001B[31m";   
+    public static final String GREEN = "\u001B[32m"; 
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";  
+    public static final String CYAN = "\u001B[36m";  
+    
+    
+    public static final String BG_RED = "\u001B[41m"; 
+}
 
